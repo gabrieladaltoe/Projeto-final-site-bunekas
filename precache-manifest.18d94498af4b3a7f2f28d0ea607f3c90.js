8e222f0c182d1eb647fa1c67e0e37b7f193a0760
Gabriela Dal Toé Fortuna <gabrieladaltoe@gmail.com>
2018-12-07 10:48:07 -0200

    Updates

---
+++ precache-manifest.18d94498af4b3a7f2f28d0ea607f3c90.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "c808326907ff583269e863d1acffa36d",
+    "url": "/projeto-final-site-bunekas/static/media/sapato.c8083269.png"
+  },
+  {
+    "revision": "eac63760f68469f73d03",
+    "url": "/projeto-final-site-bunekas/static/css/main.ce791023.chunk.css"
+  },
+  {
+    "revision": "325d69040c354d744665",
+    "url": "/projeto-final-site-bunekas/static/js/1.325d6904.chunk.js"
+  },
+  {
+    "revision": "4a0d58b8e0b50787039d",
+    "url": "/projeto-final-site-bunekas/static/js/runtime~main.4a0d58b8.js"
+  },
+  {
+    "revision": "3d3dcda56c0ddf0990d511ddfb52f305",
+    "url": "/projeto-final-site-bunekas/static/media/boneka.3d3dcda5.png"
+  },
+  {
+    "revision": "cebc5af16bb703202292f2d1d3851cf8",
+    "url": "/projeto-final-site-bunekas/static/media/vestido-calcinha.cebc5af1.png"
+  },
+  {
+    "revision": "b01b631805f0d744b7730f604ba5984c",
+    "url": "/projeto-final-site-bunekas/static/media/cabelo.b01b6318.png"
+  },
+  {
+    "revision": "527b401d083d3e61e67703439a07d0d3",
+    "url": "/projeto-final-site-bunekas/static/media/expressao.527b401d.png"
+  },
+  {
+    "revision": "64d014c21b48ed1d8741a8f883a342b3",
+    "url": "/projeto-final-site-bunekas/static/media/tecido.64d014c2.png"
+  },
+  {
+    "revision": "eac63760f68469f73d03",
+    "url": "/projeto-final-site-bunekas/static/js/main.eac63760.chunk.js"
+  },
+  {
+    "revision": "4c87b6971b3130c807432ccfa0615f34",
+    "url": "/projeto-final-site-bunekas/static/media/turbante.4c87b697.png"
+  },
+  {
+    "revision": "f8c5c5242f118dd0dfde5817b0d9a7c6",
+    "url": "/projeto-final-site-bunekas/static/media/mapa-brasil.f8c5c524.png"
+  },
+  {
+    "revision": "dba02f21e25467699e833c6fdc6ecd13",
+    "url": "/projeto-final-site-bunekas/static/media/vol1.dba02f21.jpg"
+  },
+  {
+    "revision": "134ec4f8db640f248a32c95f2cea2136",
+    "url": "/projeto-final-site-bunekas/static/media/vol2.134ec4f8.jpg"
+  },
+  {
+    "revision": "3a2a859416a3d17bedfd8155e5fb43de",
+    "url": "/projeto-final-site-bunekas/static/media/vol3.3a2a8594.jpg"
+  },
+  {
+    "revision": "83f720ecb56972388ecad643d5deb6c6",
+    "url": "/projeto-final-site-bunekas/static/media/vol4.83f720ec.jpg"
+  },
+  {
+    "revision": "b254f57e8eb53008439b4ccd7b4bde58",
+    "url": "/projeto-final-site-bunekas/static/media/header-2.b254f57e.jpg"
+  },
+  {
+    "revision": "7b4f7f1eedbd19ebba1c26cd51a6e9cf",
+    "url": "/projeto-final-site-bunekas/index.html"
+  }
+];
